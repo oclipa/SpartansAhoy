5f7446748fd32aa7fb1a0101b9ce772a48cb416a
Steve Hall <steve@mowgli.local>
2018-11-22 12:42:33 +0200

    After taught part of course

--- SpartansAhoy/Assets/Scripts/CharacterController2D.cs
+++ SpartansAhoy/Assets/Scripts/CharacterController2D.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement; // include so we can load new scenes
+using UnityStandardAssets.CrossPlatformInput; // include so that we can use cross platform input (e.g. mobile)
 
 public class CharacterController2D : MonoBehaviour {
 
@@ -89,7 +90,7 @@ public class CharacterController2D : MonoBehaviour {
 			return;
 
 		// determine horizontal velocity change based on the horizontal input
-		_vx = Input.GetAxisRaw ("Horizontal");
+        _vx = CrossPlatformInputManager.GetAxisRaw ("Horizontal");
 
 		// Determine if running based on the horizontal movement
 		if (_vx != 0) 
@@ -119,11 +120,11 @@ public class CharacterController2D : MonoBehaviour {
 		// Set the grounded animation states
 		_animator.SetBool("Grounded", _isGrounded);
 
-		if(_isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
+		if(_isGrounded && CrossPlatformInputManager.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
         {
             DoJump();
         }
-        else if (_canDoubleJump && Input.GetButtonDown("Jump"))
+        else if (_canDoubleJump && CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             DoJump();
 
@@ -133,7 +134,7 @@ public class CharacterController2D : MonoBehaviour {
 
         // If the player stops jumping mid jump and player is not yet falling
         // then set the vertical velocity to 0 (he will start to fall from gravity)
-        if (Input.GetButtonUp("Jump") && _vy>0f)
+        if (CrossPlatformInputManager.GetButtonUp("Jump") && _vy>0f)
 		{
 			_vy = 0f;
 		}
